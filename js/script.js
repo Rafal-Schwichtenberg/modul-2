b90d77125f241ed1b9404d56833d5f32f105cf34
Rafal Schwichtenberg <svistxxx@gmail.com>
2023-02-28 23:06:12 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log("hi");
+
+let button = document.querySelector(".button");
+let button2 = document.querySelector(".button2");
+let img = document.querySelector(".img");
+button = addEventListener("click", () => 
+{img.remove();
+});
+
+
+
+
